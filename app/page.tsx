f88f20116366ce0f0d9ffef2bb2e2cdec0fadd9d
Uziel Tzab <uzieltzab8@gmail.com>
2025-03-23 10:01:16 -0600

    Se agregó el Header y el Footer a la página principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import {Header}  from "@/components/ui/header";
+import {Footer} from "@/components/ui/footer";
+
 export default function Page() {
     return (
         <div>
+            <Header />
+            <main>
+                <h1>Contenido de la página</h1>
+            </main>
+            <Footer />
+            
             
         </div>
     );
