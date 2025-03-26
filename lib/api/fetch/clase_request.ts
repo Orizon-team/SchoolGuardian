b9c903318f5ccff502db9988218317f696a833b9
Uziel Tzab <uzieltzab8@gmail.com>
2025-03-26 00:00:03 -0600

    Co-authored-by: Jhonny342 <Jhonny342@users.noreply.github.com>

--- lib/api/fetch/clase_request.ts
+++ lib/api/fetch/clase_request.ts
@@ -1,36 +1,58 @@
-const URL = "https://assists-api.onrender.com/api/clases";
+import { get } from 'http';
+import { url } from "inspector";
+import Cookies from "js-cookie";
 
-export interface ClaseData {
-    nombre_clase: string;
-    horario: string; // formato ISO (ejemplo: "2025-03-20T10:00:00Z")
-    duracion: number;
-    id_profesor: number;
-    dias: string[]; // Ejemplo: ["Lunes", "Miércoles", "Viernes"]
-}
+const URL = "https://assists-api.onrender.com/api/clases/usuario";
 
-export async function createClaseRequest(
-    claseData: ClaseData,
-    token: string
-): Promise<any | null> {
-    try {
-        const response = await fetch(`${URL}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(claseData),
-        });
+// export async function createClaseRequest(
+//     claseData: ClaseData,
+//     token: string
+// ): Promise<any | null> {
+//     try {
+//         const response = await fetch(`${URL}`, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Authorization: `Bearer ${token}`,
+//             },
+//             body: JSON.stringify(claseData),
+//         });
 
-        if (!response.ok) {
-            console.error("Error en la solicitud:", response.statusText);
-            return null;
-        }
+//         if (!response.ok) {
+//             console.error("Error en la solicitud:", response.statusText);
+//             return null;
+//         }
 
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error al realizar el fetch:", error);
-        return null;
-    }
-}
+//         const data = await response.json();
+//         return data;
+//     } catch (error) {
+//         console.error("Error al realizar el fetch:", error);
+//         return null;
+//     }
+// }
+
+// export async function GetAllClassesOfTeacher(id_profesor: number)
+// {
+//     const token = Cookies.get("token");
+//     try {
+//         const reponse = await fetch(`${URL}`, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Authorization:`Bearer ${token}`
+
+//             },
+//             body: JSON.stringify(
+//                 {
+//                     id_
+//                 }
+//             );
+
+//         })
+        
+        
+//     } catch (error) {
+        
+//     }
+
+// }
